05720a9065ae11c744be53d3b30c8b559d5a1ec8
Veksoe <Anniika-xb@live.dk>
2025-01-06 19:23:01 +0100

    Næsten færdiggjort landingpage til desktop mm.
    - Mangler nogle billeder
    - Mangler Alt-tekter
    - Fjernet knap-komponent
    - Så rettelser og clean up i css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 import { Navigation } from '../components/TheHeader.js'; // Importer Navbar-komponenten
-import { Btn } from '../components/BasicBtn.js'; // Importer Btn-komponenten
 import { TheFooter } from '../components/TheFooter.js'; // Importer Footer-komponenten
 import { Carousel } from '../components/Carousel.js'; // Importer Footer-komponenten
 
@@ -7,7 +6,6 @@ import { Carousel } from '../components/Carousel.js'; // Importer Footer-kompone
 Vue.createApp({
     components: {
         Navigation, // Registrer Navbar-komponenten
-        Btn, // Registrer Btn-komponenten
         TheFooter, // Registrer Sooter-komponenten
         Carousel
     }
