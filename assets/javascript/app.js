8584f7d3deed70c318af743eb533e270ab46cb1e
Veksoe <Anniika-xb@live.dk>
2025-01-06 14:43:51 +0100

    Opsat karousel
    - Opsat komponent til karousel
    - Opsat design til karousel
    - Mangler korrekte billeder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,15 @@
 import { Navigation } from '../components/TheHeader.js'; // Importer Navbar-komponenten
 import { Btn } from '../components/BasicBtn.js'; // Importer Btn-komponenten
 import { TheFooter } from '../components/TheFooter.js'; // Importer Footer-komponenten
+import { Carousel } from '../components/Carousel.js'; // Importer Footer-komponenten
 
 // Opret Vue-applikationen og registrer komponenterne
 Vue.createApp({
     components: {
         Navigation, // Registrer Navbar-komponenten
         Btn, // Registrer Btn-komponenten
-        TheFooter // Registrer Sooter-komponenten
+        TheFooter, // Registrer Sooter-komponenten
+        Carousel
     }
 }).mount("#app");
 
