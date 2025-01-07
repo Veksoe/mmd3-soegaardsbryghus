190899d10aa459c80cc253603b0fdbee934143c0
Veksoe <Anniika-xb@live.dk>
2025-01-07 10:31:43 +0100

    Lavet mobil version til headeren

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,11 +2,39 @@
 document.addEventListener('DOMContentLoaded', () => {
     const heroTextEl = document.querySelectorAll('.changingTextContainer .changingText');
     const scrollIndicator = document.querySelector('.scrollIndicator');
+    const burgerMenu = document.querySelector('#burgerMenu');
+    const sideMenu = document.querySelector('#sideMenu');
+    const closeMenu = document.querySelector('#closeMenu');
+    const changeTextEl = document.querySelector('changingTextContainer');
+    const navLinks = document.querySelectorAll('.topnav a, .topnavMobile  a ');
 
+
+    // Get the current URLs' pathname without the domainname 
+    const currentPath = window.location.pathname;
     let currentIndex = 0;
 
-    // Call changeText function every 8th second
-    setInterval(changeText, 6000);
+    if (changeTextEl) {
+        // Call changeText function every 8th second
+        setInterval(changeText, 6000);
+    }
+
+
+
+    // Loop through each link and add .active to the matching link(s)
+    navLinks.forEach(link => {
+        // Skip links with href="#"
+        if (link.getAttribute('href') === '#') {
+            link.classList.remove('active');
+            return;
+        }
+        // Check if the pathname matches the current pathname
+        if (link.pathname === currentPath) {
+            link.classList.add('active');
+        } else {
+            //Remove if it doesn't match
+            link.classList.remove('active');
+        }
+    });
 
 
     // Changes text on landingpage hero over time
@@ -24,25 +52,41 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 1000)
     }
 
+    if (scrollIndicator) {
+        // Hide scrollIndicator when scrolled down
+        window.addEventListener('scroll', () => {
+            // Checks, is scrollY is above 200px
+            if (window.scrollY > 200) {
+                scrollIndicator.classList.add('hidden');
+            } else {
+                scrollIndicator.classList.remove('hidden');
+            }
+        });
 
-    // Hide scrollIndicator when scrolled down
-    window.addEventListener('scroll', () => {
-        // Checks, is scrollY is above 200px
-        if (window.scrollY > 200) {
-            scrollIndicator.classList.add('hidden');
-        } else {
-            scrollIndicator.classList.remove('hidden');
+        // Adds click event for scrollIndicator
+        scrollIndicator.addEventListener('click', () => {
+            window.scrollBy({
+                top: 700, // Moves 700px down
+                left: 0,
+                behavior: 'smooth' // Makes scroll smooth
+            });
+        });
+    }
+
+    // Open side-menu
+    burgerMenu.addEventListener('click', () => {
+        sideMenu.classList.add('open');
+    });
+
+    // Close side-menu
+    closeMenu.addEventListener('click', () => {
+        sideMenu.classList.remove('open');
+    });
+
+    // CLose side-menu when clicking outside of the menu
+    document.addEventListener('click', (e) => {
+        if (!sideMenu.contains(e.target) && !burgerMenu.contains(e.target)) {
+            sideMenu.classList.remove('open');
         }
     });
-
-    // Adds click event for scrollIndicator
-    scrollIndicator.addEventListener('click', () => {
-        window.scrollBy({
-            top: 700, // Moves 700px down
-            left: 0,
-            behavior: 'smooth' // Makes scroll smooth
-        });
-    });
-
-
 });
